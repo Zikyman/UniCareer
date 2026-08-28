3ee8cf6a668cf21268063673d2618c5a958dce49
Zikyman <richardkrepelka@gmail.com>
2026-08-28 13:35:21 +0200

    chore(controllers): Include controllers and Add Swagger build

--- UniCareer.API/Program.cs
+++ UniCareer.API/Program.cs
@@ -3,45 +3,31 @@ using UniCareer.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. Zaregistrování DbContextu pro SQLite (Naše databáze)
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite("Data Source=unicareer.db"));
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// 2. Přidání podpory pro Kontroléry (Naše CRUD endpointy)
+builder.Services.AddControllers();
+
+// 3. Konfigurace Swaggeru (Dokumentace a testování API)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 4. Nastavení pro vývojové prostředí (Zobrazení Swaggeru)
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// 5. Zmapování našich kontrolérů na URL adresy
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
